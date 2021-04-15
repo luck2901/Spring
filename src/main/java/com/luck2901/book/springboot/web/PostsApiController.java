b122aa798cefbb696ff2300c26d5b64b0d700eea
한상현 <luck2901@naver.com>
2021-04-15 18:40:29 +0900

    4/15 Study

--- src/main/java/com/luck2901/book/springboot/web/PostsApiController.java
+++ src/main/java/com/luck2901/book/springboot/web/PostsApiController.java
@@ -1,11 +1,12 @@
 package com.luck2901.book.springboot.web;
 
+import com.luck2901.book.springboot.domain.posts.PostsRepository;
 import com.luck2901.book.springboot.services.posts.PostsService;
+import com.luck2901.book.springboot.web.dto.PostsResponseDto;
 import com.luck2901.book.springboot.web.dto.PostsSaveRequestDto;
+import com.luck2901.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +17,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return PostsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
     }
 }
